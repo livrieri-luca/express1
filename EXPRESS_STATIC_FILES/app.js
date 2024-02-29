bc478c5f765d6ed8e4c5647d481e83c014f5a5ba
livrieri-luca lucon <livrieri.luca@iisgalvanimi.edu.it>
2024-02-29 08:04:50 +0000

    Description of changes of commit

--- EXPRESS_STATIC_FILES/app.js
+++ EXPRESS_STATIC_FILES/app.js
@@ -1,15 +1,18 @@
-var express = require('express');
+var express= require('express');
 var app = express(); 
 const path = require('path')
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
-});
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname, 'index.html')); //__dirname : Ritorna la cartella del progetto
+ })
 app.get('/chi_sono', function (req, res) {
     res.send('<h1>io, sono luca</h1>');
 });
-app.get('/h', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-app.listen(10000, function () {
+app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname,'about.html')); //__dirname : Ritorna la cartella del progetto
+ });
+ app.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname,'sitemap.html')); //__dirname : Ritorna la cartella del progetto
+ });
+app.listen(20000, function () {
   console.log('Example app listening on port 3000!');
 });
