12f865a01cdfceac51fc77d68a7112b9c12343f9
livrieri-luca lucon <livrieri.luca@iisgalvanimi.edu.it>
2024-03-11 23:04:07 +0000

    Description of changes of commit

--- LavoroNuovo/server.js
+++ LavoroNuovo/server.js
@@ -1,56 +1,25 @@
 const express = require('express');
+const people = require('./people.json'); 
 const app = express();
-const port = 8000;
-
-app.use(express.static('public')); // Serve static files like images and CSS
-app.set('view engine', 'pug'); // Use Pug as the template engine
-
-function getProfileData(nome) {
-  if (nome === 'chiellini') {
-    return {
-      nome: nome,
-      eta: '39',
-      biografia: 'è nato a Pisa il 14 agosto 1984, ma è cresciuto a Livorno. È un ex calciatore italiano, di ruolo difensore e attualmente collaboratore tecnico del Los Angeles FC.',
-      Ruolo: 'difensore',
-    };
-  } else if (nome === 'dybala') {
-    return {
-      nome: nome,
-      eta: '30',
-      biografia: 'Paulo Exequiel Dybala è nato a Laguna Larga, Argentina, il 15 novembre 1993. È un calciatore, attaccante della Roma e della Nazionale argentina. Dybala ha vinto il Mondiale con la nazionale argentina nel 2022.',
-      Ruolo: 'Attaccante',
-    };
-  } else if (nome === 'ronaldotrasferimento') {
-    return {
-      nome: nome,
-      eta: '39',
-      biografia: 'È nato nel 1985 a Funchal da Maria Dolores dos Santos, una cuoca, e José Dinis Aveiro, un giardiniere municipale con origini capoverdiane (la nonna era originaria di São Vicente), in un momento nel quale la famiglia versava in una condizione di grave disagio economico a causa dei problemi di alcolismo del padre (che lo portarono alla morte nel 2005), con la madre inizialmente decisa ad abortire.',
-      Ruolo: 'Attaccante',
-    };
-  }
-}
-
-// Homepage
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Sito Profili' });
+  res.render('index', { title: 'Homepage' });
 });
 
-// Pagina dei profili
 app.get('/profili', (req, res) => {
-  res.render('profili', { title: 'Profili' });
+  res.render('profili', {
+    title: 'PROFILI',
+    people: people.profiles 
+  });
 });
-
-// Pagina del profilo della persona
-app.get('/profili/:persona', (req, res) => {
-  const persona = req.params.persona;
-  const profilo = getProfileData(persona);
-  if (profilo) {
-    res.render('persona', { title: `Profilo di ${persona}`, persona });
-  } else {
-    res.status(404).send('Profilo non trovato');
-  }
+app.get('/profilo', (req, res) => {
+  const person = people.profiles.find((p) => p.id === req.query.id);
+  res.render('profilo', {
+    title: ` ${person.firstname} ${person.lastname}`,
+    person,
+  });
 });
-
-app.listen(port, () => {
-  console.log(`Server in ascolto su http://localhost:${port}`);
+app.listen(4000, function () {
+ console.log('Example app listening on port 3000!');
 });
